8816b6fb2d6e6b733b3fc0133bc822b7cd0ba3a5
Rosie Sun <weijias1@andrew.cmu.edu>
2018-06-30 20:01:02 -0400

    fb

--- remote-website/imports/ui/FacebookApp/FacebookApp.js
+++ remote-website/imports/ui/FacebookApp/FacebookApp.js
@@ -3,11 +3,41 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
 export class FacebookApp extends Component {
+  handleUpvote = _id => {
+    this.props
+      .incOptionCount({
+        variables: {
+          _id
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   renderQs = questionsQuery => {
     if (questionsQuery.loading) {
       return "";
     }
-    return questionsQuery.questions.map(q => <div key={q._id}>{q.title}</div>);
+    return questionsQuery.questions.map(q => (
+      <div key={q._id}>
+        <h3>{q.title}</h3>
+        <ul>
+          {q.options.map(opt => (
+            <li>
+              {opt.title} with {opt.count} count
+              <br />
+              <button
+                className="vote"
+                onClick={() => this.handleUpvote(opt._id)}
+              >
+                vote me
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ));
   };
 
   render() {
@@ -19,6 +49,14 @@ export class FacebookApp extends Component {
     );
   }
 }
+
+const incOptionCount = gql`
+  mutation incrementCount($_id: ID!) {
+    incrementCount(_id: $_id) {
+      _id
+    }
+  }
+`;
 
 const questionsQuery = gql`
   query {
